8662e17e05bf9ba13164b6e39e956e17c6ba0377
samarth000 <99653464+samarth000@users.noreply.github.com>
2023-08-27 21:17:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1= Math.floor(Math.random()*6 ) + 1;
+var randomdiceImage = "./images/dice"+ randomNumber1 + ".png"
+document.querySelectorAll("img")[0].setAttribute("src", randomdiceImage);
+
+var randomNumber2= Math.floor(Math.random()*6 ) + 1;
+var randomdiceImage2 = "./images/dice"+ randomNumber2 + ".png"
+document.querySelectorAll("img")[1].setAttribute("src", randomdiceImage2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins"
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 wins"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw"
+}
